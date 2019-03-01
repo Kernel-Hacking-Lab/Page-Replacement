dd5c8491992a7cd20e3891be3bdd674def67616c
Shreshth Tuli <shreshthtuli@gmail.com>
2019-03-01 21:26:09 +0530

    Update run.sh

--- benchmarks/spec/run.sh
+++ benchmarks/spec/run.sh
@@ -1,7 +1,9 @@
 echo "Libquantum\n"
 cd ./libquantum/src/
-time ./make.out 1397 8
+./make.out
+time ./libquantum 100 8
 echo "MCF\n"
 cd ../../
 cd ./mcf/src/
-time ./make.out inp.in 
+./make.out
+time ./mcf inp.in
