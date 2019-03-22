9bfb0b07a9a6821c7d462ec419e7ffb6f7d8f556
Shreshth Tuli <shreshthtuli@gmail.com>
2019-03-22 06:44:30 +0530

    Updated run.h

--- benchmarks/spec/run.sh
+++ benchmarks/spec/run.sh
@@ -1,11 +1,14 @@
+vmstat > output-vm.txt
 echo "Libquantum\n"
 cd ./libquantum/src/
 chmod +x make.out
 ./make.out
-time ./libquantum 500 8
+time ./libquantum 500 8 > output-time.txt
+vmstat > output-vm.txt
 echo "MCF\n"
 cd ../../
 cd ./mcf/src/
 chmod +x make.out
 ./make.out
-time ./mcf inp.in
+time ./mcf inp.in > output-time.txt
+vmstat > output-vm.txt
